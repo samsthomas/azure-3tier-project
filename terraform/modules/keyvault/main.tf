e0f9120627da856e7ab98b7f466ba77c045acd67
Sam Thomas <samsthomas@hotmail.co.uk>
2025-11-15 20:30:36 +0000

    commenting out keyvault code

--- terraform/modules/keyvault/main.tf
+++ terraform/modules/keyvault/main.tf
@@ -10,16 +10,16 @@ resource "azurerm_key_vault" "kv" {
   tags = var.tags 
 }
 
-resource "azurerm_key_vault_access_policy" "current_user" {
-  key_vault_id = azurerm_key_vault.kv.id
-  tenant_id = var.tenant_id
-  object_id = var.current_user_object_id
+# resource "azurerm_key_vault_access_policy" "current_user" {
+#   key_vault_id = azurerm_key_vault.kv.id
+#   tenant_id = var.tenant_id
+#   object_id = var.current_user_object_id
 
-  secret_permissions = [
-    "Get", "List", "Set"
-  ]
+#   secret_permissions = [
+#     "Get", "List", "Set"
+#   ]
   
-}
+# }
 
 resource "azurerm_key_vault_access_policy" "github_oidc" {
   key_vault_id = azurerm_key_vault.kv.id
@@ -34,19 +34,3 @@ resource "azurerm_key_vault_access_policy" "github_oidc" {
   ]
   
 }
-
-# resource "azurerm_key_vault_secret" "sql_admin_password" {
-#   name = "sql-admin-password"
-#   value = var.sql_admin_password
-#   key_vault_id = azurerm_key_vault.kv.id
-
-#   lifecycle {
-#     ignore_changes = [value]
-# }
-
-
-#   depends_on = [
-#     azurerm_key_vault_access_policy.current_user,
-#     azurerm_key_vault_access_policy.github_oidc
-#   ]
-# }
