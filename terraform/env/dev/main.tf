de0727f1d7bbd368bded82641cef979183bbf2ef
Sam Thomas <samsthomas@hotmail.co.uk>
2025-11-15 19:42:12 +0000

    more formatting

--- terraform/env/dev/main.tf
+++ terraform/env/dev/main.tf
@@ -85,7 +85,7 @@ module "storage" {
 
 resource "azurerm_storage_container" "tfstate" {
   name                  = "tfstate"
-  storage_account_id   = module.storage.storage_account_id
+  storage_account_id    = module.storage.storage_account_id
   container_access_type = "private"
 }
 
