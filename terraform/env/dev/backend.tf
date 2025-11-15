9e3028f1cc1b396ad34cd0510156efced5b8f75c
Sam Thomas <samsthomas@hotmail.co.uk>
2025-11-15 18:44:40 +0000

    pushing pipeline changes

--- terraform/env/dev/backend.tf
+++ terraform/env/dev/backend.tf
@@ -3,7 +3,7 @@ terraform {
     resource_group_name  = "rg-3tier-dev"
     storage_account_name = "st3tierdevyw01jg"
     container_name       = "tfstate"
-    key                  = "dev.terraform.tfstate"
+    key = "dev.terraform.tfstate"
 
   }
 }
--- terraform/env/dev/main.tf
+++ terraform/env/dev/main.tf
@@ -4,14 +4,18 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 3.80"
+      version = "~> 4.0"
     }
     random = {
-      source  = "hashicorp/random"
+      source = "hashicorp/random"
       version = "~> 3.6"
     }
   }
 }
+provider "azurerm" {
+  features {}
+}
+
 
 resource "azurerm_resource_group" "rg" {
   name     = var.resource_group_name
