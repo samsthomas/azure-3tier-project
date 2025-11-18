c64c4838e85b40bcfd3da4d8100bb10bfa01d3ae
Sam Thomas <samsthomas@hotmail.co.uk>
2025-11-18 22:14:25 +0000

    changes to sql to test access

--- terraform/modules/sql/main.tf
+++ terraform/modules/sql/main.tf
@@ -6,7 +6,7 @@ resource "azurerm_mssql_server" "sql" {
   administrator_login           = var.admin_username
   administrator_login_password  = var.admin_password
   minimum_tls_version           = "1.2"
-  public_network_access_enabled = false
+  public_network_access_enabled = true
 
   tags = var.tags
 }
@@ -25,6 +25,13 @@ resource "azurerm_mssql_firewall_rule" "allow_app_subnet" {
   start_ip_address = cidrhost(var.app_subnet_prefix, 1)
   end_ip_address   = cidrhost(var.app_subnet_prefix, 254)
 
+}
+
+resource "azurerm_mssql_firewall_rule" "allow_azure" {
+  name             = "AllowAllAzureServices"
+  server_id        = azurerm_mssql_server.sql.id
+  start_ip_address = "0.0.0.0"
+  end_ip_address   = "0.0.0.0"
 }
 
 module "diagnostics_sql" {
