3807a973d45f94cd7da47a73fca97a48fb8fb0e0
Sam Thomas <samsthomas@hotmail.co.uk>
2025-11-18 19:00:09 +0000

    changes to the code and pipeline, code changes to add db changes, pipeline to force app restarts

--- src/backend/server.js
+++ src/backend/server.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const app = express();
+const sql = require("mssql");
+const dbConnectionString = process.env.DB_CONNECTION_STRING;
 
 const PORT = process.env.PORT;
-
-const dbConnectionString = process.env.DB_CONNECTION_STRING || "not-set";
 
 app.get("/health", (req, res) => {
     res.json({ status: "ok", time: new Date().toISOString() });
@@ -18,4 +18,39 @@ app.get("/api/message", (req, res) => {
 
 app.listen(PORT, () => {
     console.log(`Backend listening on port ${PORT}`);
+});
+
+app.get("/api/dbinit", async (req, res) => {
+    try {
+        const pool = await sql.connect(dbConnectionString);
+
+        await pool.request().query(`
+            IF NOT EXISTS (SELECT * FROM sysobjects WHERE name='Messages' AND xtypes='U')
+            CREATE TABLE Messages (
+                id INT IDENTITY(1,1) PRIMARY KEY,
+                message NVARCHAR(255),
+                created_at DATETIME DEFAULT GETDATE()
+            );
+        `);
+
+        await pool.request()
+            .input("Msg", sql.NVarChar, "Hello from backend at " + new Date().toISOString())
+            .query("INSERT INTO Messages (message) VALUES (@msg)");
+
+        res.json({ ok:true, message: "Table ready & row inserted. "});
+    } catch (err) {
+        console.log.error("DBINIT ERROR", err);
+        res.status(500).json({ ok: false, error: err.message })
+    }
+});
+
+app.get("/api/dbrows", async (req, res) => {
+    try {
+        const pool = await sql.connect(dbConnectionString);
+        const result = await pool.request().query("SELECT TOP 10 * FROM Messages ORDER By id DESC");
+        res.json({ ok: true, rows: result.recordset });
+    } catch (err) {
+        console.log.error("DBROWS ERROR:", err);
+        res.status(500).json({ ok: false, error: err.message });
+    }
 });
